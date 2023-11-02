5abbfa15ac2d9cc2d145772ceef6a9edf377f358
Tanima-062 <tyousuf141062@bscse.uiu.ac.bd>
2023-11-02 22:50:12 +0600

    :pencil2: ADD: Files

---
+++ resources/views/backend/dashboard/url_short.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>URL Shortener App</title>
+    @include('backend.layouts.styles')
+    <link rel="stylesheet" href="{{asset('backend/assets/templates/vendors/font-awesome/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('backend/assets/templates/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css')}}" type="text/css" />
+</head>
+<body>
+<div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    @include('backend.layouts.header')
+    <div class="container-fluid page-body-wrapper">
+        @include('backend.layouts.sidebar')
+        <div class="main-panel report-page">
+            <div class="content-wrapper" >
+              @include('backend.layouts.messages')
+                <h3 class="users-list-title">SHORT URL</h3>
+                <hr class="Dash-Line">
+                <div class="row">
+                  <div class="col-md-7 m-auto">
+                    <div class="card">
+                      <div class="card-body">
+                        <h2>Click the link</h2>
+                        <h2><a href="{{url('/short/'.$url->short)}}" target="_blank">{{$url->short}}</a></h2>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            <!-- main-panel ends -->
+        </div>
+    </div>
+</div>
+<!-- page-body-wrapper ends -->
+<!-- container-scroller -->
+
+@include('backend.layouts.scripts')
+
+</body>
+
+</html>
